cc407f0e04ff01bced7f61d28e07b07557e9999b
Arisa Ichikawa <pocky.love2@icloud.com>
2017-05-18 12:34:38 +0900

    第５回　じゃんけん

--- docs/player.js
+++ docs/player.js
@@ -2,38 +2,77 @@ const GU = 0;
 const CHOKI = 1;
 const PA = 2;
 
-
-
 let matches = 0;
-function action(oppornent){
-  matches = matches + 1;
-  const remainder = matches % 2;
-  const isEven = remainder == 0;
-  console.log(`${matches} % 2 = ${remainder} : ${isEven}`);
-    if(isEven){
-    return GU;
-  }
-  return PA;
+
+function countup() {
+matches = matches + 1
+return matches;
 }
 
-function abs(value){
-  let result = value;
-  if(result < 0){
-    result = result * -1;
-  }
-  return result;
+// チョキ大好きマンに対するアクション
+function actionAgainstChokiDaisukiman() {
+return GU;
 }
 
-    
-
-
-
-function distance(value1, value2){
-let result = 0;
-result = value2 - value1;
-result=abs(result);
+// 表か裏かマンに対するアクション
+function actionAgainstOmoteUra() {
+const isEven = matches % 2 == 0;
+let result = PA;
+if (isEven) {
+result = GU;
+}
 return result;
 }
+
+// 帰ってきた表か裏かマンに対するアクション
+function actionAgainstOmoteUra2() {
+const toReturnChoki = matches % 3 == 0;
+const toReturnPa = matches % 3 == 1 ;
+let result = GU;
+
+if (toReturnChoki) {
+result = CHOKI;
+}else 
+  if (toReturnPa) {
+    result = PA;
+  } else {
+    result = GU;
+  }
+
+return result;
+}
+
+/*
+対戦相手名前まとめ
+チョキ大好きマン： fighter::choki-lover
+表か裏かマン：fighter::odd-even
+帰ってきた表か裏かマン：fighter::on-third
+*/
+
+function action(oppornent) {
+console.log(oppornent);
+let result = GU;
+countup();
+if (oppornent == "fighter::choki-lover") {
+result = actionAgainstChokiDaisukiman();
+} else if (oppornent == "fighter::odd-even" ) {
+result = actionAgainstOmoteUra2();
+} else {
+  if (oppornent == "fighter::rotation") {
+
+  }else{
+    result = actionAgainstOmoteUra2();
+  }
+}
+
+return result;
+}
+
+
+  
+
+
+
 
 
   
